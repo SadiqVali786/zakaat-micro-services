c203ad42998ff4f0ff17b2576a00873cb9caf660
SadiqVali786 <rebirth4vali@gmail.com>
2025-03-03 14:30:55 +0530

    configured tailwind.config.ts to recognize the packages/ui folder tailwind code

--- apps/nextjs-web-app/tailwind.config.ts
+++ apps/nextjs-web-app/tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"],
   presets: [sharedConfig]
 };
 
