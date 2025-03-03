a6ba4b741529f1f473d75900724fbcaa86bf7d74
SadiqVali786 <rebirth4vali@gmail.com>
2025-03-03 18:16:22 +0530

    configured zustand

---
+++ packages/zustand/src/bear-store.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+
+type BearState = {
+  bears: number;
+  increase: (by: number) => void;
+};
+
+const useBearStore = create<BearState>()(
+  immer(
+    devtools(
+      persist(
+        (set) => ({
+          bears: 0,
+          increase: (by) =>
+            set((state) => {
+              state.bears += by;
+            })
+        }),
+        {
+          name: "bear-storage"
+        }
+      )
+    )
+  )
+);
+
+export default useBearStore;
